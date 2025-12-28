9bd56b9b390b0616f39a43f3323f33ea004b76ea
Vidhyashree <158534940+VVidhyashree@users.noreply.github.com>
2025-12-28 09:52:49 +0530

    hard.sql

---
+++ northwind.db/hard.sql
@@ -0,0 +1,38 @@
+->Show the employee's first_name and last_name, a "num_orders" column with a count of the orders taken, and a column called "Shipped" that displays "On Time" if the order shipped_date is less or equal to the required_date, "Late" if the order shipped late, "Not Shipped" if shipped_date is null.
+Order by employee last_name, then by first_name, and then descending by number of orders.
+
+SELECT
+  e.first_name,
+  e.last_name,
+  COUNT(o.order_id) As num_orders,
+  (
+    CASE
+      WHEN o.shipped_date <= o.required_date THEN 'On Time'
+      WHEN o.shipped_date > o.required_date THEN 'Late'
+      WHEN o.shipped_date is null THEN 'Not Shipped'
+    END
+  ) AS shipped
+FROM orders o
+  JOIN employees e ON e.employee_id = o.employee_id
+GROUP BY
+  e.first_name,
+  e.last_name,
+  shipped
+ORDER BY
+  e.last_name,
+  e.first_name,
+  num_orders DESC
+
+->Show how much money the company lost due to giving discounts each year, order the years from most recent to least recent. Round to 2 decimal places
+Select 
+YEAR(o.order_date) AS 'order_year' , 
+ROUND(SUM(p.unit_price * od.quantity * od.discount),2) AS 'discount_amount' 
+
+from orders o 
+JOIN order_details od ON o.order_id = od.order_id
+JOIN products p ON od.product_id = p.product_id
+
+group by YEAR(o.order_date)
+order by order_year desc;
+
+->
